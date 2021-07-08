cb97384725e8e49e15472c0007cadc1189d26f42
dekel241 <dekel241@gmail.com>
2021-07-09 02:29:42 +0300

    Rearrange main file

--- main.cpp
+++ main.cpp
@@ -5,17 +5,15 @@
 #include <optional>
 
 int main(void) {
-//    std::string line;
-//    std::optional<struct blell> blelll;
-//    while(true) {
-//        std::getline(std::cin, line);
-//        std::cout << line << '\n';
-//        blell = parseMessage(line);
-//    }
-    std::optional<struct blell> blell = parseMessage("LL Data: 0e 07 03 00 04 00 0a 17 00");
-    std::cout << *blell << std::endl;
-    blell = parseMessage("LL Data: 02 07 03 00 04 00 0a 03 00");
-    std::cout << *blell << std::endl;
+    std::string line;
+     //blelll;
+
+    while(true) {
+        std::getline(std::cin, line);
+        std::cout << line << '\n';
+        std::optional<struct blell> blell = parseMessage(line);
+        std::cout << *blell << std::endl << std::endl;
+    }
 
     return 0;
 }
