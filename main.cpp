9de1390336793f6830a1d618d844af14f7cf6420
dekel241 <dekel241@gmail.com>
2021-07-13 00:41:51 +0300

    Change program to read from named pipe

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 #include <iostream>
 #include <string>
 #include <optional>
+#include <fstream>
+#include <sys/stat.h>
 
 int main(void) {
+    //Create named pipe with default permissions
+    mkfifo("btlejack_output", 0666);
+    std::fstream pipe("btlejack_output");
     std::string line;
 
     while(true) {
-        std::getline(std::cin, line);
+        std::getline(pipe, line);
         std::optional<struct blell> blell = parseMessage(line);
         if(blell) {
             std::cout << *blell << std::endl << std::endl;
